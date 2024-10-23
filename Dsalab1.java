f554f636a244018fff661e2dca52123b678e0dbe
mahamkhan240 <maham1823khan@gmail.com>
2024-10-23 12:46:34 +0500

    Add files via upload

---
+++ Dsalab1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Dsalab1 {
+
+
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a positive integer n: ");
+            int n = scanner.nextInt();
+
+           
+            int sum = n * (n + 1) / 2;
+
+            System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
+        }
+    }
+
+
